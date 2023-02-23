aa2d8563ca854dd60e7bff6a531f2874a2580b61
antonina <atoninasych@gmail.com>
2023-02-23 10:40:04 +0000

    updated with lambda processing

--- config.rb
+++ config.rb
@@ -2,4 +2,5 @@ require_relative 'zipper/zipper'
 require_relative 'zipper/processor'
 Zipper.config do |config|
     config.extension = '.txt'
+    config.processing = ->(content) {puts content.upcase}
 end
